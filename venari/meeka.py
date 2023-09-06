46681912dbc10ae591762c71f3c7e4a08d84422c
Jason Du <contact@dujason.com>
2023-09-06 19:39:28 -0400

    Add meeka

--- venari/meeka.py
+++ venari/meeka.py
@@ -1,34 +1,25 @@
 from .venari import Venari
-from effect import Unique, AccuracyDebuff
+from effect import Unique, Bleed
 from config import DamageType
 
 
 class Meeka(Venari):
     def basic_attack(self, target):
-        effect = self.get_opportunist_effect()
-        if self.battle_handler.has_effect(effect):
-            self.battle_handler.remove_effect(effect, self)
-            super().basic_attack(target, 30)
+        if target.has_effect_id("bleed"):
+            super().basic_attack(target, 20)
             self.messages.append(f"{self.name} dealt an empowered auto attack.")
         else:
             super().basic_attack(target)
 
-    def on_target_miss(self, target):
-        super().on_target_miss(target)
-        self.apply_effect(Unique(self.messages, "opportunist", 5, 5))
-        self.messages.append(f"{self.name}({self.level})'s gained an opportunist stack!")
-
     def use_ability(self, target):
         super().use_ability(target)
-        target.apply_effect(AccuracyDebuff(self.messages, 6, 50))
+        target.apply_effect(Bleed(self.messages, self.level, self.battle_stats.attack_damage, 6, 6))
 
         self.messages.append(f"{self.name} used its ability on {target.name}!")
 
     def on_swap_in(self, enemy_team=None):
         # Call the base class's method to reset the attack tick counter
         super().on_swap_in()
-        self.deal_damage(enemy_team[0], 20, DamageType.AD, 100)
-        enemy_team[0].apply_effect(AccuracyDebuff(self.messages, 3, 20))
-
-    def get_opportunist_effect(self):
-        return Unique(self.messages, "Opportunist", 2, 2)
+        super().basic_attack(enemy_team[0])
+        if enemy_team[0].has_effect_id("bleed"):
+            super().basic_attack(enemy_team[0])
